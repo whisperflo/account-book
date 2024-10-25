19f6f748de3d67bbeecadb5eec27390711b7d037
FlowWhisper <1790696167@qq.com>
2024-10-25 11:27:00 +0800

    添加gitee和github实现双同步

--- sync_gitee_github.sh
+++ sync_gitee_github.sh
@@ -9,10 +9,10 @@ git commit -m "$commit_message"
 
 # 推送到 Gitee
 echo "正在推送到 Gitee..."
-git push origin main  # 或者你的 Gitee 分支名称
+git push    # 或者你的 Gitee 分支名称
 
 # 推送到 GitHub
 echo "正在推送到 GitHub..."
-git push github main  # 或者你的 GitHub 分支名称
+git push github   # 或者你的 GitHub 分支名称
 
 echo "同步推送到 Gitee 和 GitHub 完成！"
